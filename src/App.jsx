dc538e5faf1879efb862c4b8d8b62aa1f12e59f9
Ibrahim KODY SANEDA <isaneda@centralpay.eu>
2021-12-26 21:10:42 +0100

    maj on pokemon details page, added stat barcharte and style

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ import { MainProvider } from "./Contexts/MainContext/MainContext";
 function App() {
   return (
     <MainProvider>
-      <div className="h-12/12 bg-blue-100">
+      <div className=" h-screen w-screen text-center bg-blue-100">
         <div>hello the is the header</div>
         <Routes>
           <Route path="/" exact element={<HomeScreen />} />
-          <Route path="/pokemon/:pokemonName" exact element={<Pokemon />} />
+          <Route path="/pokemon/:urlName" exact element={<Pokemon />} />
         </Routes>
         <div>this is the footer</div>
       </div>
--- src/containers/Pokemon.jsx
+++ src/containers/Pokemon.jsx
@@ -1,37 +1,70 @@
-import { useContext } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { MainContext } from "../Contexts/MainContext/MainContext";
+import PokemonApi from "../Api/PokemonApi/PokemonApi";
 
 const Pokemon = () => {
-  const { pokemonName } = useParams();
+  const { urlName } = useParams();
+  const [foudedPokemon, setFoundedPokemon] = useState({});
 
-  const { data } = useContext(MainContext);
+  const getPokemon = async () => {
+    const response = await PokemonApi.get(`pokemon/${urlName}`);
+    setFoundedPokemon(response.data);
+  };
 
-  const getPokemon = data.find((pokemon) => pokemon.name === pokemonName);
+  useEffect(() => {
+    getPokemon();
+  }, []);
 
-  const foundedPokemon = { ...getPokemon };
+  const { name, order, weigth, sprites, stats } = foudedPokemon;
+  console.log(name);
 
-  const { name, id, order, weight, sprites } = foundedPokemon;
   const getCss = () => {
     return {
-      backgroundImage: `url(${
-        sprites ? sprites.other.dream_world.front_default : ""
-      })`,
       backgroundSize: "contain",
       backgroundRepeat: "no-repeat",
       backgroundPosition: "center center",
       width: "100%",
-      height: "80%",
+      height: "100%",
       position: "relative",
     };
   };
-  console.log(foundedPokemon.sprites.front_default);
   return (
-    <div className="w-64 h-72 rounded-md m-auto bg-white shadow-lg">
-      <h1 className="mx-auto w-max">
-        {name} <span>No.{order}</span>
-      </h1>
-      <div style={getCss()} />
+    <div className="w-9/12 rounded-md m-auto bg-white shadow-lg p-4">
+      <h1 className="mx-auto my-8 w-max">{name}</h1>
+      <div className="flex flex-wrap">
+        <img
+          className="w-12/12 bg-contain bg-center bg-no-repeat"
+          src={sprites ? sprites.other["official-artwork"].front_default : null}
+          alt=""
+        />
+        <div>
+          <table>
+            <tbody>
+              {stats
+                ? stats.map((item) => {
+                    const barChartWidth = item.base_stat + 50;
+                    return (
+                      <tr key={item.stat.name} className=" text-left">
+                        <td>
+                          <strong className="mx-2 ">{item.stat.name}</strong>
+                        </td>
+                        <td>
+                          <div
+                            style={{ width: barChartWidth }}
+                            className="px-3 py-1 bg-orange-700 rounded-full m-1"
+                          >
+                            {item.base_stat}
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })
+                : null}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div />
       <div></div>
     </div>
   );
